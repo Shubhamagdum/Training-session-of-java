efd51aa5d7df3e00519e857206e968e4cd22c698
Shubhamagdum <shubhamagdum62559510@gmail.com>
2024-06-11 15:27:15 +0530

    Add files via upload

---
+++ FunDemo.java
@@ -0,0 +1,16 @@
+package sam.com;
+
+public class FunDemo {
+
+	public static void add() {
+		int a=10;
+		int b=20;
+		int total=a+b;
+		System.out.println("Addition : "+total);
+	}
+	public static void sub(int a,int b) {
+		int total=a-b;
+		System.out.println("Sub : "+total);
+	}
+	
+}
---
+++ Testfun.java
@@ -0,0 +1,12 @@
+package sam.com;
+
+public class Testfun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//FunDemo fd=new FunDemo();
+		FunDemo.add();
+		FunDemo.sub(30, 10);
+	}
+
+}
